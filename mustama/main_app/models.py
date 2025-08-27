f4364c4f59abd39e3f0faf18cbf7dd01584aaa18
Alderazi <qm8222606@gmail.com>
2025-08-27 16:14:24 +0300

    finished homePage and set up database

---
+++ mustama/main_app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Recitation(models.Model):
+    surahName=models.CharField(max_length=30)
+    Reciter=models.CharField(max_length=100)
+    reciterImage=models.ImageField(upload_to='main_app/static/uploads/picutres',default="")
+    audio=models.FileField(upload_to='main_app/static/uploads/audio',)
